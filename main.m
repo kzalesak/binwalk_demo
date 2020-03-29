034944c729f52475fbc2bca80a02f3009de77273
kzalesak <kzalesak@gmail.com>
2020-03-30 00:58:08 +0200

    Display mean in console

--- main.m
+++ main.m
@@ -9,3 +9,5 @@ plot(data)
 xlabel("Datapoints")
 ylabel("Generated values")
 title(sprintf("Plot of generated values. Mean: %d",data_mean))
+
+disp(sprintf("The mean, rounded to one sigfig is: %d",round(data_mean,1)))
