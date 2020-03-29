c4eea549dfbb36a9f1126f88957e5eb68bfa1a2d
jakubzalesak16 <jakub.zalesak.16@ucl.ac.uk>
2020-03-29 22:41:29 +0200

    Initial commit

---
+++ generate_data.m
@@ -0,0 +1,2 @@
+data = random('norm',0,1,500);
+save data/my_dataset data;
---
+++ main.m
@@ -0,0 +1,6 @@
+% LOAD A DATASET, PLOT IT AND DISPLAY ITS MEAN
+
+loaded = load('data/my_dataset');
+
+figure(1)
+plot(loaded.data)
