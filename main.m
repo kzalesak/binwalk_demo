887fb776377881ff5cef47db293e3cb1210f1904
kzalesak <kzalesak@gmail.com>
2020-03-30 01:10:19 +0200

    Nicer blue in plot

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ data = generate_data(1500);
 data_mean = mean(data);
 
 figure(1)
-plot(data)
+plot(data,"b-")
 xlabel("Datapoints")
 ylabel("Generated values")
 title(sprintf("Plot of generated values. Mean: %d",data_mean))
