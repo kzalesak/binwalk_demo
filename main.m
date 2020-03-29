b257e3431fa33a598d761ddd18a022c10a1567b4
kzalesak <kzalesak@gmail.com>
2020-03-30 01:00:09 +0200

    Add visual line representing mean

--- main.m
+++ main.m
@@ -10,4 +10,10 @@ xlabel("Datapoints")
 ylabel("Generated values")
 title(sprintf("Plot of generated values. Mean: %d",data_mean))
 
+%plot mean visually as well
+hold on
+mean_vec = ones(size(data)) * data_mean;
+plot(mean_vec)
+hold off
+
 disp(sprintf("The mean, rounded to one sigfig is: %d",round(data_mean,1)))
