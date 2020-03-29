c8680e69361b1930964f7dd9a905fd69cda01e69
Jakub Zalesak (work) <kzalesak+work@gmail.com>
2020-03-29 23:20:03 +0100

    Display mean

--- main.m
+++ main.m
@@ -2,6 +2,10 @@
 
 % loaded = load('data/my_dataset');
 data = generate_data(500);
+mean = mean(data);
 
 figure(1)
 plot(data)
+xaxis("Datapoints")
+yaxis("Generated values")
+title(spintf("Plot of generated values. Mean: %d",mean))
